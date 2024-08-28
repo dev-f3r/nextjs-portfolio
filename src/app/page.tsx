ad8d306bbf5629431044c50627d47b18b7757e3a
Fernando Castro <devfercastro@gmail.com>
2024-08-28 16:57:54 -0300

    refactor(Home): Welcome section moved appart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Content from "./ui/content";
+import Welcome from "./ui/welcome";
 
 export default function Home() {
   return (
-    <main className="flex w-screen">
-      <div className="w-screen h-screen flex items-center justify-center">
-        <h1 className="text-3xl font-bold w-100">{"Hello, I'm Fernando"}</h1>
-      </div>
+    <main className="flex w-screen flex-col">
+      <Welcome />
+      <Content />
     </main>
   );
 }
