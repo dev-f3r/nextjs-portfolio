2ef9a7b4c1c73aefddf98ee678e25107e3fd995f
devfercastro <devfercastro@gmail.com>
2024-08-28 22:01:24 +0000

    build: Prettier + Eslint setup

--- src/app/ui/content.tsx
+++ src/app/ui/content.tsx
@@ -1,9 +1,14 @@
+"use client";
+
+import Navbar from "./navbar";
+
 export default function Content() {
   return (
     <div className="h-screen flex items-center justify-center" id="content">
       <div className="h-full w-full p-20">
         <h2 className="text-xl font-medium">{"I'm a software developer."}</h2>
+        <Navbar />
       </div>
     </div>
-  )
+  );
 }
--- src/app/ui/navbar.tsx
+++ src/app/ui/navbar.tsx
@@ -3,13 +3,23 @@ export default function Navbar() {
     <nav>
       <div className="navbar-container">
         <ul className="navbar-links">
-          <li className="navbar-link"><a href="#Welcome">Welcome</a></li>
-          <li className="navbar-link"><a href="#about">About</a></li>
-          <li className="navbar-link"><a href="#experience">Experience</a></li>
-          <li className="navbar-link"><a href="#skills">Skills</a></li>
-          <li className="navbar-link"><a href="#contact">Contact</a></li>
+          <li className="navbar-link">
+            <a href="#Welcome">Welcome</a>
+          </li>
+          <li className="navbar-link">
+            <a href="#about">About</a>
+          </li>
+          <li className="navbar-link">
+            <a href="#experience">Experience</a>
+          </li>
+          <li className="navbar-link">
+            <a href="#skills">Skills</a>
+          </li>
+          <li className="navbar-link">
+            <a href="#contact">Contact</a>
+          </li>
         </ul>
       </div>
     </nav>
-  )
+  );
 }
