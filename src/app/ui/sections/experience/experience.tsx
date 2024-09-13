0e1eb36899443341bb46794cb590366337ebc05a
devfercastro <devfercastro@gmail.com>
2024-09-13 02:08:48 +0000

    feat(experience section): Add styles

--- src/app/ui/sections/experience/experience.tsx
+++ src/app/ui/sections/experience/experience.tsx
@@ -1,5 +1,12 @@
+import ExperienceItem from "./experienceItem"
+import { experienceList } from "@/app/lib/placeholder-data"
+
 export default function ExperienceSection() {
   return (
-    <div>Experience section.</div>
+    <div className="grid grid-cols-2 grid-rows-2 gap-5">
+      {Object.entries(experienceList).map(([key, value]) => (
+        <ExperienceItem key={key} {...value} />
+      ))}
+    </div>
   )
 }
--- src/app/ui/sections/experience/experienceItem.tsx
+++ src/app/ui/sections/experience/experienceItem.tsx
@@ -1,5 +1,33 @@
-export default function ExperienceItem() {
+import { JobExperience } from "@/app/lib/definitions";
+import { ArrowTopRightOnSquareIcon, FireIcon as PlaceholderIcon } from "@heroicons/react/24/solid";
+
+export default function ExperienceItem(job: JobExperience) {
   return (
-    <div>Experience item.</div>
-  )
+    <div className="cursor-pointer border-2 hover:shadow-lg rounded-xl py-5 px-10 group">
+      <header className="flex flex-row w-full justify-between mb-2">
+        <div className="flex flex-col">
+          <h1 className="text-lg font-medium">
+            {job.title} - {job.company}
+          </h1>
+          <p className="text-base text-gray-800 font-semibold">
+            {job.from} - {job.to}
+          </p>
+        </div>
+        <ArrowTopRightOnSquareIcon
+          height={25}
+          width={25}
+          className="opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+        />
+      </header>
+      <div className="flex-col gap-5">
+        <p className="text-sm mb-2">{job.description}</p>
+        <div className="flex gap-5">
+          {job.techStack.map((item) => (
+            // TODO: Add technology icon
+            <PlaceholderIcon height={25} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
